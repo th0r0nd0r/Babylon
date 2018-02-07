fb5baf329640ed4d6dcdc5ee5434f92b0f29ecd3
th0r0nd0r <pjfritch1@gmail.com>
2018-02-07 13:06:36 -0800

    play with nets

--- learning/babylon.js
+++ learning/babylon.js
@@ -7,15 +7,11 @@ var createScene = function () {
 
   // This creates a basic Babylon Scene object (non-mesh)
   var scene = new BABYLON.Scene(engine);
+scene.enablePhysics();
 
-  // This creates and positions a free camera (non-mesh)
-  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+  var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 2, 30, BABYLON.Vector3.Zero(), scene);
 
-  // This targets the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero());
-
-  // This attaches the camera to the canvas
-  camera.attachControl(canvas, true);
+camera.attachControl(canvas, true);
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
   var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
@@ -23,21 +19,69 @@ var createScene = function () {
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
-  // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
-  var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, scene);
+  var subdivisions = 20;
+var groundWidth = 8;
 
-  // Move the sphere upward 1/2 its height
-  sphere.position.y = 2;
+var distanceBetweenPoints = groundWidth / subdivisions;	
+
+var clothMat = new BABYLON.StandardMaterial("texture3", scene);
+  clothMat.diffuseTexture = new BABYLON.Texture("http://i.imgur.com/2HklR1L.jpg", scene);
+clothMat.zOffset = -20;
+clothMat.backFaceCulling = false;
 
   // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
-  var ground = BABYLON.Mesh.CreateGround("ground1", 16, 16, 2, scene);
+  var ground = BABYLON.Mesh.CreateGround("ground1", groundWidth, groundWidth, subdivisions - 1, scene, true);
+var ground2 = BABYLON.Mesh.CreateGround("ground2", groundWidth, groundWidth, 2, scene, false);
+ground.material = clothMat;
+  ground2.material = clothMat;
 
-scene.enablePhysics();
+  ground.rotation.x = Math.PI / 2;
+  // ground2.rotation.y = Math.PI;
+  
+var positions = ground.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+var spheres = [];
+for (var i = 0; i < positions.length; i = i + 3) {
+  var v = BABYLON.Vector3.FromArray(positions, i);
+  
+  var s = BABYLON.MeshBuilder.CreateSphere("s" + i, { diameter: 0.1 }, scene);
+  s.position.copyFrom(v);
+  spheres.push(s);
+}
 
-sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
-ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+function createJoint(imp1, imp2) {
+  var joint = new BABYLON.DistanceJoint({
+    maxDistance: distanceBetweenPoints
+  })
+  imp1.addJoint(imp2, joint);
+}
 
-sphere.physicsImpostor.setLinearVelocity(new BABYLON.Vector3(1, 0, 1));
+//create the impostors
+spheres.forEach(function (point, idx) {
+  var mass = 1;
+  point.physicsImpostor = new BABYLON.PhysicsImpostor(point, BABYLON.PhysicsImpostor.ParticleImpostor, { mass: mass }, scene);
+  point.physicsImpostor.setLinearVelocity( new BABYLON.Vector3(4,12,0));
+      if (idx >= subdivisions) {
+    createJoint(point.physicsImpostor, spheres[idx - subdivisions].physicsImpostor);
+    if (idx % subdivisions) {
+      createJoint(point.physicsImpostor, spheres[idx - 1].physicsImpostor);
+    }
+  }
+});
+
+ground.registerBeforeRender(function () {
+  var positions = [];
+  spheres.forEach(function (s) {
+    positions.push(s.position.x, s.position.y, s.position.z);
+
+  });
+  ground.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
+  ground.refreshBoundingInfo();
+});
+
+var bigSphere = BABYLON.MeshBuilder.CreateSphere("bigSphere", { diameter: 4, segments: 16 }, scene);
+bigSphere.position.y = -3;
+bigSphere.physicsImpostor = new BABYLON.PhysicsImpostor(bigSphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 0 }, scene);
+
 
   return scene;
 
